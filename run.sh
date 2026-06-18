104194989c85103b62b407da332a360fe9183d2b
Mat-002 <m.cialdella1@studentipoliba.it>
2026-06-18 10:05:34 +0200

    first commit

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 xhost +
 docker run -it --rm --net host --ipc host --privileged \
-    --gpus all \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v ~/.Xauthority:/root/.Xauthority \
     -e DISPLAY=$DISPLAY \
