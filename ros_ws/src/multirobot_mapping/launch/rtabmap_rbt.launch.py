b19107df4b2f2c88a21d41fca46a344376ccef3b
DanielePS-9 <danielesomma289@gmail.com>
2026-06-21 11:09:26 +0200

    modfiche rtab

--- ros_ws/src/multirobot_mapping/launch/rtabmap_rbt.launch.py
+++ ros_ws/src/multirobot_mapping/launch/rtabmap_rbt.launch.py
@@ -21,6 +21,15 @@ def generate_launch_description():
             parameters=[{
                 'use_sim_time': use_sim_time,
                 
+                # 1. Sincronizzazione flessibile (Obbligatorio per Gazebo)
+                'approx_sync': True,
+                'queue_size': 30,
+
+                # 3. Decimazione della PointCloud (Il salvavita)
+                'Grid/DepthDecimation': '4', # Prende 1 punto ogni 4, abbattendo il carico del 90%
+                'Grid/MaxObstacleHeight': '1.5', # Ignora il soffitto della stanza
+                'Grid/MinClusterSize': '20', # Filtra il rumore visivo
+
                 # Sostituite le f-string con PathJoinSubstitution per risolvere l'oggetto LaunchConfiguration
                 'frame_id': PathJoinSubstitution([namespace, 'base_footprint']),
                 'odom_frame_id': PathJoinSubstitution([namespace, 'odom']),
@@ -30,19 +39,22 @@ def generate_launch_description():
                 'subscribe_rgb': False,
                 'subscribe_scan': False,
                 'subscribe_rgbd': False,
+                'subscribe_stereo' : False,
                 
                 # Abilitiamo la sottoscrizione diretta alla PointCloud2
-                'subscribe_point_cloud': True,
+                'subscribe_scan_cloud': True,
                 
                 # Ottimizzazioni per la simulazione (Evita lag eccessivo)
+                'RGBD/Enabled': 'false', 
+                'Reg/Strategy': '1',
                 'Rtabmap/DetectionRate': '1.0', # Aggiorna la mappa 1 volta al secondo (riduce CPU)
                 'RGBD/ProximityBySpace': 'true',
-                'RGBD/AngularUpdate': '0.05',
-                'RGBD/LinearUpdate': '0.05',
+                'RGBD/AngularUpdate': '0.0',
+                'RGBD/LinearUpdate': '0.0',
                 'Mem/IncrementalMemory': 'true', # true = SLAM (mappa e localizza), false = solo localizzazione
             }],
             remappings=[
-                ('point_cloud', 'stereo_camera/pointCloud'),
+                ('scan_cloud', 'stereo_camera/pointCloud'),
                 ('tf', '/tf'),
                 ('tf_static', '/tf_static')
             ],
